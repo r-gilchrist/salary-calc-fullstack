26e541ed5b8efa5db2f395b058e2caf746739aa4
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2023-03-12 16:31:00 +0000

    example frontend retrieval from salary endpoint via `POST` method

--- backend/salary_calc_fastapi/endpoints/salary_calculation.py
+++ backend/salary_calc_fastapi/endpoints/salary_calculation.py
@@ -1,7 +1,20 @@
 from fastapi_restful import Resource
+from fastapi import Request
+from salary_calc_fastapi.models.salary import Salary
+from pydantic import BaseModel
+
+
+class SalaryRequest(BaseModel):
+    reference_salary: float
 
 
 class SalaryCalculation(Resource):
-
     def get(self):
         return {"hello": "world"}
+
+    def post(self, example: SalaryRequest):
+        salary = Salary(reference_salary=example.reference_salary)
+        return {
+            "Original salary": example.reference_salary,
+            "Net salary": salary.get_net(),
+        }
--- frontend/src/hello_world.ts
+++ frontend/src/hello_world.ts
@@ -1,5 +1,14 @@
-console.log("Hi!");
-fetch("http://localhost:5000/salary")
+fetch("http://localhost:5000/salary", {
+  method: "POST",
+
+  body: JSON.stringify({
+    reference_salary: 47000,
+  }),
+
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
   .then(function (response) {
     return response.json();
   })
