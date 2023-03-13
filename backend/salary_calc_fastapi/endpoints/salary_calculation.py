982704308499789c11049c208b720aaa2debadcd
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2023-03-13 12:50:25 +0000

    Add student loan to `POST` endpoint

--- backend/salary_calc_fastapi/endpoints/salary_calculation.py
+++ backend/salary_calc_fastapi/endpoints/salary_calculation.py
@@ -6,12 +6,14 @@ from pydantic import BaseModel
 class SalaryRequest(BaseModel):
     reference_salary: float
     pension_contribution: float
+    student_loan_type: str
 
 
 class SalaryResponse(BaseModel):
     reference_salary: float
     income_tax: float
     national_insurance: float
+    student_loan: float
     pension: float
     net_salary: float
 
@@ -21,11 +23,16 @@ class SalaryCalculation(Resource):
         return {"hello": "world"}
 
     def post(self, example: SalaryRequest) -> SalaryResponse:
-        salary = Salary(reference_salary=example.reference_salary, percentage_employee=example.pension_contribution)
+        salary = Salary(
+            reference_salary=example.reference_salary,
+            percentage_employee=example.pension_contribution,
+            loan_type=example.student_loan_type,
+        )
         return SalaryResponse(
             reference_salary=salary.reference_salary,
             income_tax=salary.income_tax,
             national_insurance=salary.national_insurance,
+            student_loan=salary.student_loan,
             pension=salary.pension,
-            net_salary=salary.net_salary
+            net_salary=salary.net_salary,
         )
