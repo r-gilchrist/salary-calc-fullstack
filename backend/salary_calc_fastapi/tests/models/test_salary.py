cfca4143ca9f015c2a57c9d3c85f19188eb4cc19
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2023-03-13 12:19:45 +0000

    tests now assert values are `almost` equal
    By asserting values are almost equal, rather than exactly equal, rounding floating point numbers can be avoided

--- backend/salary_calc_fastapi/tests/models/test_salary.py
+++ backend/salary_calc_fastapi/tests/models/test_salary.py
@@ -6,12 +6,12 @@ class TestSalaryModel(TestCase):
 
     def test_no_deductions(self):
         salary = Salary(reference_salary=5000)
-        self.assertEqual(5000, salary.get_net())
+        self.assertEqual(5000, salary.net_salary)
 
     def test_basic_rate(self):
         salary = Salary(reference_salary=25000)
-        self.assertEqual(21022.40, salary.get_net())
+        self.assertEqual(21022.40, salary.net_salary)
 
     def test_higher_rate(self):
         salary = Salary(reference_salary=75000)
-        self.assertEqual(52549.40, salary.get_net())
+        self.assertEqual(52549.40, salary.net_salary)
