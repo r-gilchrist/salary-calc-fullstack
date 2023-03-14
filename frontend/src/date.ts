164fb151d7ef5ce855699e89ed096cdc2dc30458
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2023-03-14 12:12:56 +0000

    change date input to separate month and year text inputs

--- frontend/src/date.ts
+++ frontend/src/date.ts
@@ -1,6 +1,19 @@
-const dateInput = document.getElementById("date-input") as HTMLInputElement | null;
+const dateYearInput = document.getElementById("date-year-input") as HTMLInputElement | null;
+const dateMonthInput = document.getElementById("date-month-input") as HTMLInputElement | null;
+
+const date = new Date();
 
 export function set_date() {
-    if (dateInput == null) return;
-    dateInput.valueAsDate = new Date();
+  set_year();
+  set_month();
+}
+
+function set_year() {
+  if (dateYearInput == null) return;
+  dateYearInput.value = String(date.getFullYear());
+}
+
+function set_month() {
+  if (dateMonthInput == null) return;
+  dateMonthInput.value = String(date.getMonth() + 1);
 }
